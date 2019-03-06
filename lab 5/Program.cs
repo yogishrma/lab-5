6be8cc249914a300fd180aab19b19258414eb0a8
visualstudio <visualstudio@122-PC40>
2019-03-06 11:19:40 -0500

    lab 5

--- lab 5/Program.cs
+++ lab 5/Program.cs
@@ -1,15 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace lab_5
+﻿namespace DelegatesAndEvents
+//student name yogesh yogesh
+// student id c0722784
+//csd3354 Section 2
+//Assignment 2
+// march 6,2019
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
+        }
+    }
+
+    public class DelegateExercises
+    {
+        public delegate int MyDelegate();
+
+        void Method1()
+        {
+            System.Console.WriteLine("MyDelegate");
+        }
+
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
         }
     }
 }
